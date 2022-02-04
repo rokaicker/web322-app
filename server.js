5fbf0d819d4c4a7c7d4a3e3977040a09ea778468
Rohan Kaicker <rkaicker@myseneca.ca>
2022-02-04 18:38:14 -0500

    changed variables to const, added routes and 404 page

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 app.use(express.static("public")); // Allows us to declare a folder as "static" --> unchanging files that are required for the site content. 
 
-var path = require("path");
-
+const path = require("path");
+const blogService = require("./blog-service.js")
 var HTTP_PORT = process.env.PORT || 8080;
 
 
@@ -12,8 +12,8 @@ var HTTP_PORT = process.env.PORT || 8080;
 function onHttpStart(){
     console.log("Express http server listening on: " + HTTP_PORT);
 }
-app.listen(HTTP_PORT, onHttpStart); // Displays port the server is listening on. Callback function "onHttpStart" is called once the "app" starts listening to the port.
 
+app.listen(HTTP_PORT, onHttpStart); // Displays port the server is listening on. Callback function "onHttpStart" is called once the "app" starts listening to the port.
 
 /*
 
@@ -27,10 +27,31 @@ Note that the HANDLER function can be an anonymous arrow function. Handler funct
 
 */
 
+// Redirect user to "/about" route
 app.get("/", (req,res)=>{
     res.redirect('/about');
 });
 
+// Send the about.html file to display the webpage
 app.get("/about", (req,res)=>{
     res.sendFile(path.join(__dirname,"/views/about.html"));
 });
+
+app.get("/blog", (req,res) => {
+    res.send("<h1>This confirms that the route to /blog is set up correctly </h1>")
+});
+
+app.get("/posts", (req,res) => {
+    res.send("<h1>This confirms that the route to /posts is set up correctly</h1>")
+});
+
+app.get("/categories", (req,res) => {
+    res.send("<h1>This confirms that the route to /categories is set up correctly</h1>")
+});
+
+
+// Send 404 status if user is trying to go an invalid route
+app.use((req,res) => {
+    res.status(404).send("Page Not Found");
+});
+
