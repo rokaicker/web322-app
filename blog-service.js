4f990654c9231691a023ec87e46d6ad8bc58fc3a
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-19 14:46:24 -0400

    sequelize configuration

--- blog-service.js
+++ blog-service.js
@@ -1,3 +1,14 @@
+const Sequelize = require('sequelize');
+var sequelize = new Sequelize('d3r91ar12fb75v', 'qrnjbgtcvuqffg','91d8eae680b3688ee904163b5171e3b77e5e79a8f5c81b22e426712dc351b8e4', {
+    host: 'ec2-3-231-254-204.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: {rejectUnauthorized: false}
+    },
+    query: {raw:true}
+});
+
 module.exports.initialize = () => {
     return new Promise ((resolve,reject) => {
         reject();
