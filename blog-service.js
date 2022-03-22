29046fc0bfd849c4d70f81fe803089f6c0d74820
Rohan Kaicker <rkaicker@myseneca.ca>
2022-03-22 15:58:03 -0400

    corrected getPostByID() - using "findOne()" instead of findAll()

--- blog-service.js
+++ blog-service.js
@@ -131,7 +131,7 @@ module.exports.getPostsByMinDate = (minDateStr) => {
 
 module.exports.getPostById = (id) => {
     return new Promise((resolve,reject) => {
-        Post.findAll({
+        Post.findOne({
             where: {
                 id: id
             }
