6036b3fb808baca05ea9687eea8791eede5eb600
Rohan Kaicker <rkaicker@myseneca.ca>
2022-04-03 19:31:05 -0400

    initialize() definition

--- auth-service.js
+++ auth-service.js
@@ -4,10 +4,30 @@ var Schema = mongoose.Schema;
 
 // Schema Setup
 var assignSchema = new Schema({
-    "userName" : String,
+    "userName" : {
+        "type": String,
+        "unique" : true
+    },
     "password": String,
     "email" : String,
     "loginHistory" : [{"dateTime" : Date}, {"userAgent" : String}]
 });
 
 let User; // to be defined on new connection see initialize
+
+// initialize() creates connection to MongoDB in Atlas
+//      Creates connection inside of a promise, if connection is successful promise is resolved().
+//      Otherwise the promise is rejected()
+module.exports.initialize = () => {
+    return new Promise ((resolve,reject) => {
+        let db = mongoose.createConnection();
+        db.on('error', (err) => {
+            reject(err);    // reject promise with provided error
+        });
+        db.once('open', () => {
+            User = db.model("users", userSchema);
+            resolve();
+        })
+    });
+};
+
