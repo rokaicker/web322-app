18aaf49ec864cfd78fd2a85594d3d705305457da
Rohan Kaicker <rkaicker@myseneca.ca>
2022-04-04 00:45:12 -0400

    Code Cleanup

--- server.js
+++ server.js
@@ -10,45 +10,37 @@
 *  GitHub Repository URL: https://github.com/rokaicker/web322-app
 *
 ********************************************************************************/ 
+// Required Modules
+const blogService = require(__dirname + "/blog-service.js");    //
+const express = require("express");                             // Express to be used as our web framework
+const path = require("path");                                   // Provides utilties for working with file/ directory pathes
+const multer = require("multer");                               // Node.js middleware for handling multipart/form-data (primary use = uploading files)
+const cloudinary = require("cloudinary").v2;                    // Cloudinary is used for image/ video hosting
+const streamifier = require("streamifier");                     // Allows us to convert buffer/ string into readable stream
+const stripJs = require('strip-js');                            // Strips JavaScript code from HTML text (ex. for safety reasons, prevent malicious scripts from running)
+const exphbs = require('express-handlebars');                   // Express handlebars view engine 
+const { appendFileSync } = require("fs");                       // TBD
+const authData = require(__dirname + "/auth-service.js");       // 
+const clientSessions = require("client-sessions");              // Middleware to implement sessions in cookies
 
-const authData = require(__dirname + "/auth-service.js");
-const clientSessions = require("client-sessions");
-// Client Sessions middleware setup
-app.use(clientSessions({
-    cookieName: "session",
-    secret: "rohan_web322_assign6",
-    duration: 2 * 60 * 1000,    // 2 minutes original duration
-    activeDuration: 60 * 1000   // 1 minute extension
-}));
-// Below middleware ensures templates have access to session object
-app.use((req,res,next) => {
-    res.locals.session = req.session;
-    next();
-})
-// Below function checks if user is logged in, if not they will be redirected to /login route
-function ensureLogin(req,res,next) {
-    if(!req.session.user) {
-        res.redirect("/login");
-    } else {
-        next();
-    }
-}
+// Providing value for HTTP_PORT for local webserver
+const HTTP_PORT = process.env.PORT || 8080;
 
-const express = require("express");
+// Express App Setup
 const app = express();
 app.use(express.static("public"));              // Allows us to declare a folder as "static" --> unchanging files that are required for the site content. 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true}));   // Allows us to recognize incoming objects as strings/ arrays
+app.use(function(req,res,next){                 // Fixes the display of the navigation bar based on the current active route
+    let route = req.path.substring(1);
+    app.locals.activeRoute = (route=="/") ? "/" : "/" + route.replace(/\/(.*)/,"");
+    app.locals.viewingCategory = req.query.category;
+    next();
+});
 
-const path = require("path");
-const blogService = require(__dirname + "/blog-service.js");
-var HTTP_PORT = process.env.PORT || 8080;
-
-// Multer setup
-const multer = require("multer");
+// Multer Setup
 const upload = multer();
 
 // Cloudinary Setup
-const cloudinary = require("cloudinary").v2;
 cloudinary.config({
     cloud_name:"dydbpvpc6",
     api_key:"894538757627284",
@@ -56,15 +48,7 @@ cloudinary.config({
     secure:true
 });
 
-const streamifier = require("streamifier");
-
-//  Strip-Js setup
-const stripJs = require('strip-js');
-
-
-// Express Handlebars Setup
-const exphbs = require('express-handlebars');
-const { appendFileSync } = require("fs");
+// Express-Handlebars Setup (includes usage of strip-js in helper function)
 app.engine('.hbs', exphbs.engine({
     extname: '.hbs',
     defaultLayout: 'main',
@@ -92,48 +76,48 @@ app.engine('.hbs', exphbs.engine({
         }
     }
 }));
-
-app.set('view engine', '.hbs');
-
+app.set('view engine', '.hbs'); // Sets our view engine to use .hbs files
 
 
-// Obtaining port app listening to
+// Client-Sessions Setup -> Includes middleware to ensure templates have access to session object, and function that checks if user is logged in (if not, user will be redirected to /login route)
+app.use(clientSessions({
+    cookieName: "session",
+    secret: "rohan_web322_assign6",
+    duration: 2 * 60 * 1000,    // 2 minutes original duration
+    activeDuration: 60 * 1000   // 1 minute extension
+}));
+app.use((req,res,next) => {
+    res.locals.session = req.session;
+    next();
+})
+function ensureLogin(req,res,next) {
+    if(!req.session.user) {
+        res.redirect("/login");
+    } else {
+        next();
+    }
+}
+
+// Function that reports the port that the app listening to
 function onHttpStart(){
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
 
-/*
-
-Redirecting user from the "/" route (aka endpoint) to the "/about" route. Routing = how app responds to client request at specific route/ endpoint. 
-Endpoints are URI ("Uniform Resource Identifier" aka path). Routes can have one/ more handler functions which are executed when route is matched. 
-
-Routing has the following structure:
-
-app.METHOD(PATH,HANDLER) --> app = express instance, METHOD = HTTP request method, PATH = endpoint/ route, HANDLER = function executed when route is matched.
-Note that the HANDLER function can be an anonymous arrow function. Handler functions = callback functions. 
-
-*/
-
-// Fixing Navigation Bar
-app.use(function(req,res,next){
-    let route = req.path.substring(1);
-    app.locals.activeRoute = (route=="/") ? "/" : "/" + route.replace(/\/(.*)/,"");
-    app.locals.viewingCategory = req.query.category;
-    next();
-});
-
-// Redirect user to "/about" route
+/* ROUTE HANDLING*/
+// Root page gets redirected to "/blog" route
 app.get("/", (req,res)=>{
     res.redirect('/blog');
 });
 
-// Send the about.html file to display the webpage
+// ABOUT PAGE
 app.get("/about", (req,res)=>{
     res.render(path.join(__dirname,"/views/about.hbs"));
 });
 
-// This will fetch all the published posts
+
+
+// BLOG PAGE
 app.get('/blog', async (req, res) => {
 
     // Declare an object to store properties for the view
@@ -181,7 +165,6 @@ app.get('/blog', async (req, res) => {
     res.render("blog", {data: viewData});
 
 });
-
 app.get('/blog/:id', async (req, res) => {
 
     // Declare an object to store properties for the view
@@ -232,6 +215,9 @@ app.get('/blog/:id', async (req, res) => {
     res.render("blog", {data: viewData})
 });
 
+
+
+// POSTS PAGE
 // This will fetch posts either based on category, date, or all posts 
 app.get("/posts", ensureLogin, (req,res) => {
     if (req.query.category){
@@ -266,7 +252,6 @@ app.get("/posts", ensureLogin, (req,res) => {
         })
     }
 });
-
 // This will get posts based on the post ID value
 app.get("/posts:value", ensureLogin, (req,res) => {
     blogService.getPostById(req.params.value).then((data) => {
@@ -275,28 +260,12 @@ app.get("/posts:value", ensureLogin, (req,res) => {
         res.json({message: err});
     })
 })
-
-// This will fetch the different post categories
-app.get("/categories", ensureLogin, (req,res) => {
-    blogService.getCategories().then((data) => {
-        console.log(data.length);
-        if (data.length > 0){
-            res.render("categories", {categories: data});
-        } else {
-            res.render("categories", {message: "no results"});
-        }
-    }).catch((err) => {
-        res.render("categories",{message:err});
-    })
-});
-
 // This will simply send the addPost.html file to the /posts/add route
 app.get("/posts/add", ensureLogin, (req,res) => {
     blogService.getCategories()
     .then((data) => res.render("addPost", {categories: data}))
     .catch((err) => res.render("addPost", {categories: []}));
 });
-
 app.post("/posts/add", ensureLogin, upload.single("featureImage"), (req,res) => {
     let streamUpload = (req) => {
         return new Promise((resolve,reject) => {
@@ -322,22 +291,35 @@ app.post("/posts/add", ensureLogin, upload.single("featureImage"), (req,res) => 
         blogService.addPost(req.body);
     }).then(res.redirect("/posts"));
 });
-
 app.get("/posts/delete/:id", ensureLogin, (req,res) => {
     blogService.deletePostById(req.params.id)
     .then(res.redirect("/posts"))
     .catch((err) => res.status(500).send("Unable to Remove Post / Post not found"));
 })
 
+
+
+// CATEGORIES PAGE
+// This will fetch the different post categories
+app.get("/categories", ensureLogin, (req,res) => {
+    blogService.getCategories().then((data) => {
+        console.log(data.length);
+        if (data.length > 0){
+            res.render("categories", {categories: data});
+        } else {
+            res.render("categories", {message: "no results"});
+        }
+    }).catch((err) => {
+        res.render("categories",{message:err});
+    })
+});
 app.get("/categories/add", ensureLogin, (req,res) => {
     res.render(path.join(__dirname, "/views/addCategory.hbs"));
 });
-
 app.post("/categories/add", ensureLogin, (req,res) => {
     blogService.addCategory(req.body)
     .then(res.redirect("/categories"));
 });
-
 app.get("/categories/delete/:id", ensureLogin, (req,res) => {
     blogService.deleteCategoryById(req.params.id)
     .then(res.redirect("/categories"))
@@ -345,11 +327,14 @@ app.get("/categories/delete/:id", ensureLogin, (req,res) => {
 })
 
 
+// 404 PAGE
 // Send 404 status if user is trying to go an invalid route
 app.use((req,res) => {
     res.status(404).render("404");
 });
 
+
+// Runs on web page initialization
 blogService.initialize()
 .then(authData.initialize())
 .then(() => {
